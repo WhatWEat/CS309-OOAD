ddd081d5f92854cbf680d2cff07587f000153260
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-09-19 22:40:29 +0800

    initialization

---
+++ src/main/java/com/example/projecthelper/ProjectHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHelperApplication.class, args);
+    }
+
+}

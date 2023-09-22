0b5d8f8c269690676ecb7adabec30dc258362876
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-09-22 15:53:14 +0800

    创建了不同角色的控制器，完善了热部署，引入了数据库

---
+++ src/main/java/com/example/projecthelper/controller/AdministratorController.java
@@ -0,0 +1,7 @@
+package com.example.projecthelper.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdministratorController {
+}
---
+++ src/main/java/com/example/projecthelper/controller/TeacherController.java
@@ -0,0 +1,14 @@
+package com.example.projecthelper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tea")
+public class TeacherController {
+    @GetMapping("/test")
+    public String get(){
+        return "hello231313";
+    }
+}

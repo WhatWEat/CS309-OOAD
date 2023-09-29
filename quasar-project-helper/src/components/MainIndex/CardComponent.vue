07bc44fb836baba07406fe5ee01aec734e88fe83
WhatWeEat <1053246332@qq.com>
2023-09-29 10:36:21 +0800

    add cards

--- quasar-project-helper/src/components/MainIndex/CardComponent.vue
+++ quasar-project-helper/src/components/MainIndex/CardComponent.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-pa-md row ">
     <q-card class="my-card">
-      <q-card-section class="bg-green text-white">
-        <div class="text-h6">System Announcements</div>
+      <q-card-section class="bg-green-8 text-white">
+        <div class="text-h6">{{title}}</div>
       </q-card-section>
-      <q-card-actions align="around">
-      </q-card-actions>
-    </q-card>
-    <q-card class="my-card">
-      <q-card-section class="bg-green text-white">
-        <div class="text-h6">New Information</div>
+      <q-separator dark inset />
+      <q-card-section>
+        {{caption}}
       </q-card-section>
-      <q-card-actions align="around">
-      </q-card-actions>
     </q-card>
   </div>
 </template>
 
 <script setup lang="ts">
-
+export interface CardInfoProps {
+  title: string;
+  caption: string;
+  link: string;
+  icon: string;
+}
+withDefaults(defineProps<CardInfoProps>(), {
+});
 </script>
 
 <style lang="sass" scoped>
 .my-card
-  width: 50vh
-  max-width: 50%
+  width: 80vh
 </style>

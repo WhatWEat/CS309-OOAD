57da9036179c33991453d6952e98990c1f84e307
yyyyellowssss <yellow_s@outlook.com>
2023-10-01 15:34:35 +0800

    这是登录界面

---
+++ my-project-frontend/src/views/welcome/Main.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="text-align: center;margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div style="font-size: 25px;font-weight: bold">这里是登录成功进入的主页面</div>
+    </div>
+  </div>
+</template>

fef9ad3518d984c7e6fb4a97249517f550dab99b
WhatWeEat <1053246332@qq.com>
2023-10-02 13:21:28 +0800

    完善了layout

---
+++ quasar-project-helper/src/pages/testPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col-8">two thirds</div>
+    <div class="col-4">one sixth</div>
+    <div class="col">fills remaining available space</div>
+    <div class="col-auto">auto size based on content and available space</div>
+    <div class="col-auto">auto size based on content and available space</div>
+    <div class="col">fills remaining available space</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>

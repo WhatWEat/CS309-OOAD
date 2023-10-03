9aad2ae5341d762b7b2676298fc631393150a63e
Li Weihao <12110415@mail.sustech.edu.cn>
2023-10-04 00:40:56 +0800

    制定Group管理框架设计

---
+++ quasar-project-helper/src/layouts/GroupLayout/GroupTeacherLayout.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "GroupTeacherLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ quasar-project-helper/src/pages/GroupPage/GroupTeacherPage.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "GroupTeacherPage"
+}
+</script>
+
+<style scoped>
+
+</style>

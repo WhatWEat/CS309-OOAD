9c45083c242dfad83cf1f4f255aaf24c2b0162af
Li Weihao <12110415@mail.sustech.edu.cn>
2023-10-15 20:44:48 +0800

    修复了route中的一些问题

--- quasar-project-helper/src/router/routes.ts
+++ quasar-project-helper/src/router/routes.ts
@@ -53,38 +53,6 @@ const routes: RouteRecordRaw[] = [
     path: '/forgotpassword',
     component: () => import('pages/ForgotPassword.vue')
   },
-
-  {
-    path: '/student/Assignment',
-    component: () => import('pages/AssignmentPage/AssignmentStudent.vue'),
-    children: [
-      {
-        path: '/personal', component: () => import('pages/AssignmentPage/AssignmentStudent_Personal.vue'),
-        children: [
-          {
-            path: '/mengbi1/:assignmentId',
-            component: () => import('pages/AssignmentPage/AssignmentDetail.vue')
-          }
-        ]
-      },
-
-      {
-        path: '/group', component: () => import('pages/AssignmentPage/AssignmentsStudent_Group.vue'),
-        children: [
-          {
-            path: '/mengbi2/:assignmentId',
-            component: () => import('pages/AssignmentPage/AssignmentDetail.vue')
-          }
-        ]
-      },
-    ]
-  },
-  {
-    path: '/teacherAssignment',
-    component: () => import('pages/AssignmentPage/AssignmentTeacher.vue')
-  },
-
-
   // Always leave this as last one,
   // but you can also remove it
   {

75f1e76d4e0f6347c6d920100505bcc2cafde2bc
lll3207 <105045321+lll3207@users.noreply.github.com>
2023-11-07 00:12:04 +0800

    文件部分

--- src/main/java/com/example/projecthelper/service/AssignmentService.java
+++ src/main/java/com/example/projecthelper/service/AssignmentService.java
@@ -23,6 +23,8 @@ import org.apache.poi.ss.usermodel.*;
 
 import java.util.*;
 
+import static com.example.projecthelper.util.FileUtil.manageTableFile;
+
 @Service
 public class AssignmentService {
     @Autowired
@@ -253,80 +255,16 @@ public class AssignmentService {
         }
     }
 
+
+    //这个方法是读取文件批量更新成绩
     public void updateGrades (String filepath,long assignmentId){
         List<Long> submitterList = new ArrayList<>();
         List<Float> gradeList = new ArrayList<>();
         manageTableFile(filepath,submitterList,gradeList);
-
         submittedAssMapper.updateGrades(submitterList,gradeList,assignmentId);
     }
 
-    //此方法从指定位置获取一个xls文件，并且读取第一张工作表,xlsx可能有问题，还在调试
-    public static void manageTableFile(String filePath, List<Long> submitterList, List<Float>gradeList) {
-        try {
-            FileInputStream file = new FileInputStream(filePath);
-//                FileInputStream file = new FileInputStream(new File(""));
-            Workbook workbook;
-            if (filePath.endsWith(".xls")) {
-                workbook = new HSSFWorkbook(file);
-            } else if (filePath.endsWith(".xlsx")) {
-                workbook = new XSSFWorkbook(file);
-            }else {
-                System.out.println("Invalid file format. Only .xls and .xlsx files are supported.");
-                return;
-            }
-            Sheet sheet = workbook.getSheetAt(0);
-            int idColumn = -1;
-            int gradeColumn = -1;
-
-            // 遍历第一行，查找“id”和“grade”列头所在的位置
-            Row headerRow = sheet.getRow(0);
-            for (int i = 0; i < headerRow.getLastCellNum(); i++) {
-                Cell cell = headerRow.getCell(i);
-                String cellValue = cell.getStringCellValue().trim();
-                if (cellValue.equalsIgnoreCase("id")) {
-                    idColumn = i;
-                } else if (cellValue.equalsIgnoreCase("grade")) {
-                    gradeColumn = i;
-                }
-            }
-
-            if (idColumn == -1 || gradeColumn == -1) {
-                System.out.println("Name or grade column not found.");
-                return;
-            }
-
-            // 存储数据的数组
 
 
-
-            // 遍历所有行（除第一行外），将每一行的name和grade值存入相应的数组
-
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                Row dataRow = sheet.getRow(i);
-                long id = (long) dataRow.getCell(idColumn).getNumericCellValue();
-                float grade =(float) dataRow.getCell(gradeColumn).getNumericCellValue();
-                submitterList.add(id);
-                gradeList.add( grade);
-            }
-
-
-
-
-//            System.out.println(Arrays.toString(names));
-//            System.out.println(Arrays.toString(grades));
-
-            file.close();
-            workbook.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //这个方法用于检测文件的拓展名是否为指定的拓展名
-    public static boolean hasExtension(File file, String extension) {
-        String fileName = file.getName();
-        return fileName.endsWith("." + extension);
-    }
 
 }

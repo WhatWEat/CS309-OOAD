7bd43b06596f64f9302381b897101de93147d59e
WhatWeEat <1053246332@qq.com>
2023-11-07 23:25:30 +0800

    完成对person project的优化

---
+++ quasar-project-helper/src/components/PersonIndex/ProjectExten.vue
@@ -0,0 +1,162 @@
+<template>
+  <q-expansion-item
+    popup
+    header-class="bg-blue-1"
+  >
+    <template v-slot:header>
+      <q-item-section>
+
+        <q-item-label class="text-h6">{{ project.projectName }}</q-item-label>
+        <q-item-label caption>
+          <q-badge rounded floating :color="isGroup ? 'green' : 'red'"/>
+          {{ project.teacherName }}
+        </q-item-label>
+      </q-item-section>
+    </template>
+
+    <q-separator/>
+    <q-card>
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="des">
+          {{ project.projectDescription }}
+        </q-tab-panel>
+
+        <q-tab-panel name="group">
+          <div class="text-h6">
+            <q-avatar>
+
+              <q-icon color="blue" name="groups"></q-icon>
+            </q-avatar>
+            <span>
+              {{ leader.name }}
+              <q-tooltip anchor="center right" self="center left" class="bg-blue">
+                <q-badge color="blue" v-for="skill in leader.skills" :key="skill">
+                  {{ skill }}
+                </q-badge>
+              </q-tooltip>
+            </span>
+
+            <q-badge rounded color="red" :label="GroupMember.length + 1"/>
+          </div>
+          <q-separator/>
+          <q-list dense class="row">
+            <q-item class="flex-center" clickable v-ripple v-for="member in GroupMember"
+                    :key="member.id"
+                    :href="`/person/${member.id}`">
+              <q-item-label>
+                {{ member.name }}
+                <q-tooltip class="bg-blue">
+                  <q-badge color="blue" v-for="skill in member.skills" :key="skill">
+                    {{ skill }}
+                  </q-badge>
+                </q-tooltip>
+              </q-item-label>
+            </q-item>
+          </q-list>
+        </q-tab-panel>
+
+        <q-tab-panel name="skill">
+          <div class="row">
+            <el-tag
+              v-for="(tag,index) in skillTags"
+              :key="tag"
+              class="q-pa-xs"
+              closable
+              :disable-transitions="false"
+              @close="handleClose(tag)"
+              :type="tagsColor[index % tagsColor.length]"
+            >
+              {{ tag }}
+            </el-tag>
+            <el-input
+              v-if="inputVisible"
+              ref="InputRef"
+              v-model="inputTagsValue"
+              class="q-pa-xs"
+              @keyup.enter="handleInputConfirm"
+              @blur="handleInputConfirm"
+            />
+            <el-button v-else class="q-pa-xs" size="small" @click="showInput">
+              + New Tag
+            </el-button>
+
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
+
+      <q-separator/>
+
+      <q-tabs
+        v-model="tab"
+        dense
+        class="bg-blue-9 text-white"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="des" label="Description"/>
+        <q-tab name="group" label="Group"/>
+        <q-tab name="skill" label="Skills"/>
+      </q-tabs>
+    </q-card>
+  </q-expansion-item>
+</template>
+
+<script setup lang="ts">
+import {projectProps, GroupMember} from "src/composables/comInterface";
+import {defineProps, nextTick, ref} from 'vue';
+import {ElInput} from "element-plus";
+
+const tab = ref<string>('des')
+const props = defineProps<{
+  project: projectProps
+}>()
+
+// group
+const isGroup = ref<boolean>(true)
+const GroupMember = ref<GroupMember[]>([{name: 'andy', id: 1, skills: ['java','cpp']}, {name: 'andy1', id: 2, skills: ['springboot','vue']},
+  {name: 'andy2', id: 3, skills: ['python']}])
+const leader = ref<GroupMember>()
+setGroup()
+
+// skill
+const inputVisible = ref<boolean>(false)
+const skillTags = ref<string[]>(['java', 'springboot'])
+const inputTagsValue = ref<string>('')
+const InputRef = ref<InstanceType<typeof ElInput>>()
+const tagsColor = ref<("success" | "info" | "warning" | "danger")[]>([
+  'success',
+  'info',
+  'warning',
+  'danger',
+]);
+
+//group method
+function setGroup() {
+  leader.value = GroupMember.value.shift()
+  isGroup.value = GroupMember.value.length > 0;
+}
+
+// skill method
+const handleClose = (tag: string) => {
+  skillTags.value.splice(skillTags.value.indexOf(tag), 1)
+}
+const showInput = () => {
+  inputVisible.value = true
+  nextTick(() => {
+    if (InputRef.value && InputRef.value.input) {
+      InputRef.value.input.focus()
+    }
+  })
+}
+const handleInputConfirm = () => {
+  if (inputTagsValue.value) {
+    skillTags.value.push(inputTagsValue.value)
+  }
+  inputVisible.value = false
+  inputTagsValue.value = ''
+}
+</script>
+
+<style scoped>
+
+</style>
--- quasar-project-helper/src/pages/PersonPages/PersonProjects.vue
+++ quasar-project-helper/src/pages/PersonPages/PersonProjects.vue
@@ -1,93 +1,34 @@
 <template>
   <div class="row justify-center">
-    <q-list class="q-ma-sm q-mt-md col-lg-8 col-md-10 col-xs-10">
-      <q-expansion-item
-        v-for="(mail, index) in mail_data" :key="index"
-        style="border-radius: 10px"
-        popup
-        header-class="bg-white"
-      >
-        <template v-slot:header>
-          <q-item-section>
-            <q-item-label>{{ mail.name }}</q-item-label>
-            <q-item-label caption>{{ mail.date }}</q-item-label>
-          </q-item-section>
-        </template>
-
-        <q-separator/>
-        <q-card>
-          <q-card-section>
-            {{ mail.msg }}
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+    <q-list class="q-ma-sm q-mt-md col-lg-8 col-md-10 col-xs-11">
+      <project-exten v-for="project in project_list" :key="project.projectId"
+                     :project="project"></project-exten>
     </q-list>
   </div>
 </template>
 
 <script setup lang="ts">
 
-import ProjectCard from 'components/PersonIndex/ProjectCard.vue';
 import {onMounted, ref} from "vue";
+import {projectProps} from "src/composables/comInterface";
+import ProjectExten from "components/PersonIndex/ProjectExten.vue";
 
-const mail_data= ref([
-  {
-    name: 'Pratik Patel',
-    avatar: 'https://avatars2.githubusercontent.com/u/34883558?s=400&v=4',
-    date: 'March 12, 2019',
-  },
-  {
-    name: 'Pratik Patel',
-    avatar: 'https://avatars2.githubusercontent.com/u/34883558?s=400&v=4',
-    date: 'March 22, 2019',
-  },
-  {
-    name: 'Pratik Patel',
-    avatar: 'https://avatars2.githubusercontent.com/u/34883558?s=400&v=4',
-    date: 'March 12, 2019',
-  },
-  {
-    name: 'Winfield Stapforth',
-    avatar: 'https://cdn.quasar.dev/img/avatar6.jpg',
-    date: 'March 22, 2019',
-  },
-  {
-    name: 'Jeff Galbraith',
-    avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg',
-    date: 'March 12, 2019',
-  },
-  {
-    name: 'Jeff Galbraith',
-    avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg',
-    date: 'March 22, 2019',
-  },
-  {
-    name: 'Razvan Stoenescu',
-    avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg',
-    date: 'March 12, 2019',
-  },
-  {
-    name: 'Razvan Stoenescu',
-    avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg',
-    date: 'March 22, 2019',
-  },
-  {
-    name: 'John Doe',
-    avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-    date: 'March 12, 2019',
-  },
-  {
-    name: 'Pratik Patel',
-    avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-    date: 'March 22, 2019',
-  },
-])
-onMounted(() => {
-  const messageText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti' +
-    ' commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste' +
-    ' eveniet doloribus ullam aliquid.';
-  mail_data.value = mail_data.value.map(item => ({ ...item, msg: messageText }));
-})
+const project_list = ref<projectProps[]>()
+project_list.value = [{
+  projectId: 0,
+  projectName: "Project 1",
+  projectDescription: "11",
+  teacherId: 0,
+  teacherName: "teacher 1"
+}, {
+  projectId: 1,
+  projectName: "Project 2",
+  projectDescription: "teacher1 2",
+  teacherId: 2,
+  teacherName: "teacher 2"
+},
+
+]
 </script>
 
 <style scoped>

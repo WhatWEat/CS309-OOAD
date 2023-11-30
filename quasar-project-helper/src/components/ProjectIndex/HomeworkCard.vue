699a5ce6232425f80939e479a1362c1c2d377eed
WhatWeEat <1053246332@qq.com>
2023-11-30 18:30:29 +0800

    login

--- quasar-project-helper/src/components/ProjectIndex/HomeworkCard.vue
+++ quasar-project-helper/src/components/ProjectIndex/HomeworkCard.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-    <q-card class="q-ma-xs no-shadow" bordered style="background-color: #38b9c5">
+    <q-card
+      class="q-ma-xs no-shadow"
+      bordered
+      style="background-color: #38b9c5"
+    >
       <q-card-section class="text-h6 text-white">
         <q-avatar>
           <q-icon name="menu_book" color="white"></q-icon>
@@ -8,14 +12,22 @@
         Assignment
       </q-card-section>
       <q-card-section class="bg-white q-mb-sm q-mx-xs">
-        <q-scroll-area v-if="loadingMessage && messages.length>0" style="height: 200px">
-          <q-infinite-scroll  @load="getAss" :offset="10">
-            <q-item v-for="msg in messages" :key="msg.assignmentId" clickable v-ripple>
+        <q-scroll-area style="height: 200px">
+          <q-infinite-scroll
+            @load="getAss"
+            :offset="10"
+            v-if="loadingMessage && messages.length > 0"
+          >
+            <q-item
+              v-for="msg in messages"
+              :key="msg.assignmentId"
+              clickable
+              v-ripple
+            >
               <q-item-section>
                 <q-item-label>{{ msg.title }}</q-item-label>
-                <q-item-label caption lines="1" class="ellipsis">{{
-                    truncate(msg.description)
-                  }}
+                <q-item-label caption lines="1" class="ellipsis"
+                  >{{ truncate(msg.description) }}
                 </q-item-label>
               </q-item-section>
               <q-item-section side>
@@ -23,49 +35,54 @@
               </q-item-section>
             </q-item>
           </q-infinite-scroll>
+          <div v-else>You don't have any assignments yet.</div>
         </q-scroll-area>
-        <div v-else>
-          You don't have any assignments yet.
-        </div>
       </q-card-section>
     </q-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import {truncate, useProjectId, formatDateString} from 'src/composables/usefulFunction';
-import {onMounted, ref} from 'vue';
-import {assProps, noticeProps} from "src/composables/comInterface";
-import {api} from "boot/axios";
+import {
+  truncate,
+  useProjectId,
+  formatDateString,
+} from "src/composables/usefulFunction";
+import { onMounted, ref } from "vue";
+import { assProps } from "src/composables/comInterface";
+import { api } from "boot/axios";
 
 const messages = ref<assProps[]>([]);
-const loadingMessage = ref(false), project_id = ref(0), count = ref(0);
+const loadingMessage = ref(false),
+  project_id = ref(0),
+  count = ref(0);
 onMounted(() => {
   project_id.value = useProjectId();
-  getAss(0, () => void 0)
-})
+  getAss(0, () => void 0);
+});
 
 function getAss(index = 0, done: () => void) {
   if (index > count.value) {
-    done()
-    return
+    done();
+    return;
   }
-  api.get(`/ass-list/${project_id.value}/${count.value}/8`).then((res) => {
-    let page_ass: assProps[] = res.data.body;
-    messages.value.push.apply(messages.value, page_ass)
-    loadingMessage.value = true;
-    if (page_ass.length === 0) {
-      done()
-      return
-    }
-    count.value++;
-    done()
-  }).catch((err) => {
-    console.log('err', err)
-  })
+  api
+    .get(`/ass-list/${project_id.value}/${count.value}/8`)
+    .then((res) => {
+      let page_ass: assProps[] = res.data.body;
+      messages.value.push.apply(messages.value, page_ass);
+      loadingMessage.value = true;
+      if (page_ass.length === 0) {
+        done();
+        return;
+      }
+      count.value++;
+      done();
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- quasar-project-helper/src/pages/LoginPages/UserLogin.vue
+++ quasar-project-helper/src/pages/LoginPages/UserLogin.vue
@@ -5,18 +5,16 @@
         <q-card class="col-lg-4 col-md-5 col-sm-6 col-xs-12">
           <q-card-section>
             <q-avatar size="103px" class="absolute-center shadow-10">
-              <img src="profile.svg">
+              <img src="profile.svg" />
             </q-avatar>
           </q-card-section>
-          <q-card-section >
+          <q-card-section>
             <div class="text-center q-pt-lg">
-              <div class="col text-h6 ellipsis">
-                Log in
-              </div>
+              <div class="col text-h6 ellipsis">Log in</div>
             </div>
           </q-card-section>
 
-          <q-card-section >
+          <q-card-section>
             <q-form class="q-gutter-md">
               <q-tabs
                 v-model="loginType"
@@ -28,24 +26,69 @@
                 <q-tab name="email" label="Email" />
                 <q-tab name="phone" label="Phone" />
               </q-tabs>
-
-              <q-input
-                filled
-                v-model="loginValue"
-                :label="loginType === 'studentId' ? 'ID' : loginType === 'email' ? 'Email' : 'Phone'"
-                :rules="getLoginValueRules()"
-              />
-
-              <q-input
-                type="password"
-                filled
-                v-model="password"
-                label="password"
-                :rules="getPasswordRules()"
-                autocomplete="current-password"
-              />
-
-              <div style="display: flex; justify-content: space-between;">
+              <q-tab-panels v-model="loginType" animated class="row">
+                <q-tab-panel name="studentId" class="col-12 row">
+                  <q-input
+                    dense
+                    outlined
+                    v-model="loginValue"
+                    label="ID"
+                    class="col-12"
+                    :rules="getLoginValueRules()"
+                  />
+                  <q-input
+                    dense
+                    type="password"
+                    outlined
+                    class="col-12"
+                    v-model="password"
+                    label="password"
+                    :rules="getPasswordRules()"
+                    autocomplete="current-password"
+                  />
+                </q-tab-panel>
+                <q-tab-panel name="email" class="col-12 row">
+                  <q-input
+                    dense
+                    outlined
+                    v-model="loginEmail"
+                    label="Email"
+                    class="col-12"
+                    :suffix="selectedEmailDomain">
+                    <template v-slot:append>
+                      <q-btn-dropdown dense flat>
+                        <q-list>
+                          <q-item clickable v-close-popup v-for="(item, index) in emailDomains"
+                                  :key="index"
+                                  @click="selectEmailDomain(index)">
+                            <q-item-section>
+                              <q-item-label>{{ item }}</q-item-label>
+                            </q-item-section>
+                          </q-item>
+                        </q-list>
+                      </q-btn-dropdown>
+                    </template>
+                  </q-input>
+                  <q-input dense
+                           outline
+                           class="col-12"
+                          label="Code">
+                    <template v-slot:append>
+                      <q-btn
+                        dense
+                        flat
+                        icon="send"
+                        @click="sendEmailCode"
+                        v-if="countdown === 0"
+                      />
+                      <div v-else>
+                        {{ countdown }}s
+                      </div>
+                    </template>
+                  </q-input>
+                </q-tab-panel>
+              </q-tab-panels>
+              <div style="display: flex; justify-content: space-between">
                 <div>
                   <q-btn
                     label="login"
@@ -63,7 +106,7 @@
                   />
                 </div>
               </div>
-              <div style="display: flex; justify-content: space-between;">
+              <div style="display: flex; justify-content: space-between">
                 <div>
                   <q-btn
                     label="forgot"
@@ -74,7 +117,6 @@
                   />
                 </div>
               </div>
-
             </q-form>
           </q-card-section>
         </q-card>
@@ -84,106 +126,138 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useQuasar } from 'quasar';
-import {api} from 'boot/axios';
-import {useUserStore} from "src/composables/useUserStore";
+import { defineComponent } from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+import { api } from "boot/axios";
+import { useUserStore } from "src/composables/useUserStore";
 
 export default defineComponent({
   setup() {
-    const router = useRouter()
-    const $q = useQuasar()
-    const loginType = ref('studentId') // 默认为学号登录
-    const loginValue = ref('')
-    const password = ref('')
-
-
+    const router = useRouter();
+    const $q = useQuasar();
+    const loginType = ref("studentId"); // 默认为学号登录
+    const loginValue = ref("");
+    const password = ref("");
+    const loginEmail = ref(""), countdown = ref(0);
+    const selectedEmailDomain = ref('gmail.com')
+    const emailDomains = ref(['@gmail.com', '@yahoo.com', '@outlook.com', '@qq.com', '@sustech.edu.cn',
+      '@mail.sustech.edu.cn'])
+    function selectEmailDomain(index) {
+      selectedEmailDomain.value = emailDomains.value[index]
+    }
+    function sendEmailCode(){
+      $q.notify({
+        color: 'green',
+        textColor: 'white',
+        icon: 'mail',
+        message: '验证码已发送'
+      });
+      // 开始倒计时
+      countdown.value = 60;
+      const interval = setInterval(() => {
+        countdown.value--;
+        if (countdown.value === 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
     function goToStudentIdLogin() {
-      loginType.value = 'studentId'
+      loginType.value = "studentId";
     }
 
     function goToEmailLogin() {
-      loginType.value = 'email'
+      loginType.value = "email";
     }
 
     function goToPhoneLogin() {
-      loginType.value = 'phone'
+      loginType.value = "phone";
     }
 
     function getLoginValueRules() {
-      const rules = []
+      const rules = [];
 
-      if (loginType.value === 'studentId') {
-        rules.push((val) => val.length === 8 || 'Student ID must be 8 digits')
-      } else if (loginType.value === 'email') {
-        rules.push((val) => /.+@.+\..+/.test(val) || 'Incorrect email format')
-      } else if (loginType.value === 'phone') {
-        rules.push((val) => val.length === 11 || 'Phone number must be 11 digits')
+      if (loginType.value === "studentId") {
+        rules.push((val) => val.length === 8 || "Student ID must be 8 digits");
+      } else if (loginType.value === "email") {
+        rules.push((val) => /.+@.+\..+/.test(val) || "Incorrect email format");
+      } else if (loginType.value === "phone") {
+        rules.push(
+          (val) => val.length === 11 || "Phone number must be 11 digits"
+        );
       }
 
-      return rules
+      return rules;
     }
 
     function getPasswordRules() {
-      return [(val) => val.length >= 6 || 'The password length cannot be less than 6 digits']
+      return [
+        (val) =>
+          val.length >= 6 || "The password length cannot be less than 6 digits",
+      ];
     }
     function login() {
-      api.post('/login', {
-        key: loginValue.value,
-        value: password.value
-      }).then((res) => {
-        console.log(res.data)
-        if (res.data.statusCode === 200) {
-          localStorage.setItem('Token', res.data.jwt_token);
-          router.push('/');
-        }
-      //   不要改动以下代码
-      }).catch((err) => {
-        $q.notify({
-          message: err.response.data.msg,
-          position: 'center'
+      api
+        .post("/login", {
+          key: loginValue.value,
+          value: password.value,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.statusCode === 200) {
+            localStorage.setItem("Token", res.data.jwt_token);
+            router.push("/");
+          }
+          //   不要改动以下代码
+        })
+        .catch((err) => {
+          $q.notify({
+            message: err.response.data.msg,
+            position: "center",
+          });
+          console.log(err);
+          console.log(loginValue);
+          console.log(password);
         });
-        console.log(err)
-        console.log(loginValue)
-        console.log(password)
-      })
-      console.log('登录:', loginValue.value, password.value)
+      console.log("登录:", loginValue.value, password.value);
     }
 
     function goToRegister() {
-      router.push('/Register')
+      router.push("/Register");
     }
 
     function goToForgotPassword() {
-      router.push('/ForgotPassword')    }
+      router.push("/ForgotPassword");
+    }
 
     return {
       loginType,
       loginValue,
+      loginEmail,
+      selectedEmailDomain,
+      emailDomains,
       password,
+      countdown,
+      selectEmailDomain,
       goToStudentIdLogin,
       goToEmailLogin,
       goToPhoneLogin,
       getLoginValueRules,
       getPasswordRules,
       login,
+      sendEmailCode,
       goToRegister,
-      goToForgotPassword
-    }
-  }
-})
+      goToForgotPassword,
+    };
+  },
+});
 </script>
 
 <style>
 .bg-image {
-  background-image: url('https://p8.itc.cn/images01/20210707/653f5d1b05cc4a3caabd0e15e676f7be.png');
+  background-image: url("https://p8.itc.cn/images01/20210707/653f5d1b05cc4a3caabd0e15e676f7be.png");
   background-size: cover;
   backdrop-filter: blur(8px);
 }
-
-
-
-
 </style>
